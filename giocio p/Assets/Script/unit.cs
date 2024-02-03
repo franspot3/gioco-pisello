47a30cbaea2da7e9a63ee182b95d1e1f1c0d576d
franspot3 <158494117+franspot3@users.noreply.github.com>
2024-02-03 15:24:40 +0100

    Update unit.cs

--- giocio p/Assets/Script/unit.cs
+++ giocio p/Assets/Script/unit.cs
@@ -2,17 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+string frocio;
+public int 531_641;
+
 public class unit : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
+
+
+
 }
