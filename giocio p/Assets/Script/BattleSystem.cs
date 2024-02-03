8a1b5af51ac863ba3c06b5d861a496b36a98edd6
tekgiamp <fran05.morella@gmail.com>
2024-02-03 15:42:08 +0100

    bho

---
+++ giocio p/Assets/Script/BattleSystem.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Gamestate{Start, PlayerTurn, EnemyTurn, Won, Lost }
+public class BattleSystem : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+}
--- giocio p/Assets/Script/unit.cs
+++ giocio p/Assets/Script/unit.cs
@@ -5,7 +5,23 @@ using UnityEngine;
 public class unit : MonoBehaviour {
     public int MaxHp;
     public int CurrentHp;
-    public float MaxMana;
-    public float CurrentMana;
-        
+    public int MaxMana;
+    public int CurrentMana;
+    public int Armor;
+    public int CurrentArmor;
+    public int MagicAmor;
+    public int CurrentMagicArmor;
+    public int Attack;
+    public int CurrentAttack;
+    public int MagicAttack;
+    public int CurrentMagicAttack;
+    public int Speed;
+    public int CurrentSpeed;
+    public int AttackSpeed;
+    public int CurrentAttackSpeed;
+    public int Accuracy;
+    public int CurrentAccuracy;
+    public int Evasion;
+    public int CurrentEvasion;
+
 }
