65db7a1c77f85d6cbaabab2633dfa6b6a7d953e1
tekgiamp <fran05.morella@gmail.com>
2024-02-03 15:23:31 +0100

    test

--- giocio p/Assets/Script/unit.cs
+++ giocio p/Assets/Script/unit.cs
@@ -2,17 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class unit : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
+public class unit : MonoBehaviour {
+    public int MaxHp;
+    public int CurrentHp;
+    public float MaxMana;
+    public float CurrentMana;
         
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
